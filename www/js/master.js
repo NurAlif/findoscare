08923f28486559b3028df463adc5cc537555828e
NurAlif <nuralif.ilyasa@gmail.com>
2016-10-26 11:30:18 +0700

    add master js

--- www/js/master.js
+++ www/js/master.js
@@ -14,23 +14,32 @@ img.src = "js/game/box.png";
 
 var speed;
 
-var imgZonk = new Image();
+var imgScare = new Image();
 var imgJackpot = new Image();
 
-imgZonk.src = "js/game/scr.png";
+imgScare.src = "js/game/scr.png";
 imgJackpot.src = "js/game/a.png";
 
 var content = new Array();
 
 content[0] = null;
-content[1] = imgZonk;
+content[1] = imgScare;
 content[2] = imgJackpot;
 
+var globalMove = function(){
+    var randVar1,randVar2;
+    randVar1 = Math.floor(Math.random() * count);
+    do{
+        randVar2 = Math.floor((Math.random() * 100) + 1);
+    }while(randVar2 === randVar1);
+    boxes[randVar1].move(randVar2);
+    
+
+};
 var boxes = new Array();
 
 var globalDisplay = function(){
     for(i = 0; i < count; i++){
-            build();
             boxes[i].display();
         }
 };
@@ -38,9 +47,9 @@ var globalDisplay = function(){
 function startgame() {
     master.start();
     gameInit();
+    build();
     globalDisplay();    
-    reveal();
-    toMoveGlobal();
+    master.move();
     
 }
 var layout = new Array();
@@ -51,11 +60,11 @@ var gameInit = function(){
     startWidth = (devWidth - layWidth) / 2;
     layHeight = devHeight * (25/100);
     startHeight = (devHeight - layHeight) / 2;
+    genFibo();
 };
 
 var base = function(dirx){
     this.x = dirx;
-    this.posx = this.x;
     this.size_X = layWidth * (1 / count);
     this.size_Y = (((layWidth / count) * img.height) / img.width);
     this.update = function(){
@@ -64,8 +73,11 @@ var base = function(dirx){
 };
 
 var build = function(){
-    layout[boxes.length] = new base(startWidth + (( layWidth / count ) * boxes.length));
-    boxes[boxes.length] = new box(layout[boxes.length].x, layout[boxes.length].size_X, layout[boxes.length].size_Y, boxes.length );
+    for(i = 0; i < count; i++){
+        
+        layout[boxes.length] = new base(startWidth + (( layWidth / count ) * boxes.length));
+        boxes[boxes.length] = new box(layout[boxes.length].x, layout[boxes.length].size_X, layout[boxes.length].size_Y, boxes.length );
+    }
 };
 
 var count;
@@ -80,96 +92,29 @@ var master = {
     },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    move : function(){
+        genMove();
+        this.moving = setInterval(timeMove,20);
+        console.log("move function complete");
     }
 };
 
-var runtimeGlb;
 
-var getTimeFold = function(){
-    var fold, ctn;
-    fold = 1;
-    ctn = 1;
-    while(fold < boxes[0].height){
-        ctn++;
-        fold += ctn;
-        fold = fold * 1;
-    }
-    return ctn;
-};
 
-var reveal = function(){
-    valFold = startHeight;
-    valSquare = 1;
-    timeFold = getTimeFold();
-    runtimeGlb = setInterval(timeReveal, 20);
-};
-
-var deReveal = function(){
-    valFold = startHeight;
-    valSquare = 1;
-    timeFold = getTimeFold();
-    runtimeGlb = setInterval(timeDereveal, 20);
-};
-
-var valFold, valSquare;
-
-var timeFold;
-
-var timeDereveal = function(){
-    if(timeFold > 0){
-        master.clear();
-        timeFold--;
-        if(timeFold > Math.floor(timeFold/2)){
-
-            valSquare += 1;
-            valFold += valSquare;
-            for(val = 0; val < boxes.length; val++ ){
-                boxes[val].reveal(valFold);
-            }
-        }else{
-            valSquare -= 1;
-            valFold += valSquare;
-            for(val = 0; val < boxes.length; val++ ){
-                boxes[val].reveal(valFold);
-            }
-        }
-    }else{
-        clearInterval(runtimeGlb);
+var genFibo = function(){
+    for(i = 0; i >= 100; i++){
+        fibonaci[i] = getFibo(i);
     }
 };
 
-var timeReveal = function(){
-    if(timeFold > 0){
-        master.clear();
-        timeFold--;
-        if(timeFold > Math.floor(timeFold/2)){
-            
-            valSquare -= 1;
-            valFold += valSquare;
-            for(val = 0; val < boxes.length; val++ ){
-                boxes[val].reveal(valFold);
-            }
-        }else{
-            valSquare += 1;
-            valFold += valSquare;
-            for(val = 0; val < boxes.length; val++ ){
-                boxes[val].reveal(valFold);
-            }
-        }
-    }else{
-        clearInterval(runtimeGlb);
-    }
-};
-
-var deltaTime = 10;
+var deltaMove;
 
 var runtimeMove;
 
 var box = function(dirx, width_O, height_O,base) {
     this.base = base;
-    this.target;
-    this.mx = dirx;
-    this.my = startHeight;
+    this.y = startHeight;
     this.x = dirx;
     this.width = width_O;
     this.height = height_O;
@@ -177,16 +122,77 @@ var box = function(dirx, width_O, height_O,base) {
     this.ctx = master.canvas.getContext("2d");
     this.display = function(){
         
-        this.ctx.drawImage(img, this.x, startHeight, this.width,this.height);
+        this.ctx.drawImage(img, this.x, this.y, this.width,this.height);
     };
-    this.move = function(x){
+    this.move = function(mx,my){
         master.clear();
-        this.mx = x;
-        this.my = this.y;
-        this.ctx.drawImage(img, this.mx, this.my, this.width,this.height);
+        this.x = mx;
+        this.y = my;
+        this.ctx.drawImage(img, mx, my, this.width,this.height);
     };
     
     this.reveal = function(valAscend){
-        this.ctx.drawImage(img, this.x, valAscend, this.width,this.height);
+        this.ctx.drawImage(img, this.x, valAscend, this.width, this.height);
+    };
+    this.dereveal = function(valDescend){
+        this.ctx.drawImage(img,this.x, valDescend, this.width, this.height);
     };
 };
+
+var fibonaci = new Array();
+function getFibo(sumHalf){
+    var val,first,second,count;
+    first = 1;
+    second = 1;
+    count = 0;
+    for(;count <= sumHalf;){
+        count++;
+        val = first + second;
+        first = second;
+        second = val;
+    }
+    return second;
+};
+
+var firtsFixed,secondFixed;
+
+function genMove(){
+    console.log("genMove is runing");
+    var firtsTarget = Math.floor(Math.random() * boxes.length);
+    var secondTarget;
+    do{
+        secondTarget = Math.floor(Math.random() * boxes.length);
+    }while(secondTarget === null || secondTarget === firtsTarget);
+    console.log("Targets is " + firtsTarget+" and "+secondTarget);
+    if(boxes[firtsTarget].x > boxes[secondTarget].x){
+        deltaMove = boxes[firtsTarget].x - boxes[secondTarget].x;
+        secondFixed = firtsTarget;
+        firtsFixed= secondTarget;
+    }else{
+        deltaMove = boxes[secondTarget].x - boxes[firtsTarget].x;
+        firtsFixed = firtsTarget;
+        secondFixed = secondTarget;
+    }
+    console.log("fixed target are "+firtsFixed+" and " + secondFixed);
+    console.log("DELTAMOVE IS "+deltaMove);
+};
+
+var timeMoveCont;
+
+function timeMove(){
+    console.log("timeMove is running");
+    timeMoveCont++;
+    if(boxes[firtsFixed].x >= layout[secondFixed].x){
+        clearInterval(master.moving);
+    }else{
+        boxes[firtsFixed].move(boxes[firtsFixed].x += 5,0 );
+        boxes[secondFixed].move(boxes[secondFixed].x -= 5,0 );
+    }
+    for(i = 0; i <= boxes.length; i++){
+        if( i === firtsFixed || i === secondFixed){
+            continue;
+        }else{
+            boxes[i].display();
+        }
+    }
+}
