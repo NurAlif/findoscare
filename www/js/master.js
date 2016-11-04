9969907d56344fd9d521a157781bf7af92e08b6c
NurAlif <nuralif.ilyasa@gmail.com>
2016-11-04 19:45:56 +0700

    update 2 master.js

--- www/js/master.js
+++ www/js/master.js
@@ -1,7 +1,18 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Nur Alif Ilyasa  2016
+ * 
+ * 
+ * Where Is My Face ? 
+ * 
+ *     Ver. 1.0.0
+ * 
+ * nuralif.ilyasa@gmail.com
+ * 
+ * https://github.com/NurAlif/findoscare.git
+ * 
+ * --------------###-------------  
+ * ----------###--###----------- editting these lines are fully allowed
+ * -------###--------###--------
  */
 
 var devWidth = document.documentElement.clientWidth;
@@ -12,13 +23,11 @@ var layWidth,layHeight,startWidth,startHeight;
 var img = new Image();
 img.src = "js/game/box.png";
 
-var speed;
-
 var imgScare = new Image();
 var imgJackpot = new Image();
 
 imgScare.src = "js/game/scr.png";
-imgJackpot.src = "js/game/a.png";
+imgJackpot.src = "js/game/trl.png";
 
 var content = new Array();
 
@@ -45,25 +54,26 @@ var globalDisplay = function(){
 };
 
 function startgame() {
+    stsrting = true;
     master.start();
     gameInit();
     build();
-    globalDisplay();    
+    globalDisplay();
     master.move();
-    
 }
 var layout = new Array();
 
-var gameInit = function(){
+
+function gameInit(){
     count = 5;
     layWidth = devWidth * (82/100);
     startWidth = (devWidth - layWidth) / 2;
     layHeight = devHeight * (25/100);
     startHeight = (devHeight - layHeight) / 2;
-    genFibo();
 };
 
-var base = function(dirx){
+
+function base(dirx){
     this.x = dirx;
     this.size_X = layWidth * (1 / count);
     this.size_Y = (((layWidth / count) * img.height) / img.width);
@@ -72,11 +82,16 @@ var base = function(dirx){
     };
 };
 
-var build = function(){
+
+function build(){
     for(i = 0; i < count; i++){
-        
         layout[boxes.length] = new base(startWidth + (( layWidth / count ) * boxes.length));
         boxes[boxes.length] = new box(layout[boxes.length].x, layout[boxes.length].size_X, layout[boxes.length].size_Y, boxes.length );
+        if(i === 0){
+            boxes[i].content = true;
+        }else{
+            boxes[i].content = false;
+        }
     }
 };
 
@@ -95,22 +110,14 @@ var master = {
     },
     move : function(){
         genMove();
-        this.moving = setInterval(timeMove,20);
-        console.log("move function complete");
-    }
-};
-
-
-
-var genFibo = function(){
-    for(i = 0; i >= 100; i++){
-        fibonaci[i] = getFibo(i);
+        this.moving = setInterval(timeMove, speed);
+        
     }
 };
 
 var deltaMove;
-
 var runtimeMove;
+
 
 var box = function(dirx, width_O, height_O,base) {
     this.base = base;
@@ -120,79 +127,356 @@ var box = function(dirx, width_O, height_O,base) {
     this.height = height_O;
     this.c = master.canvas;
     this.ctx = master.canvas.getContext("2d");
+    this.content;
     this.display = function(){
         
         this.ctx.drawImage(img, this.x, this.y, this.width,this.height);
     };
-    this.move = function(mx,my){
-        master.clear();
-        this.x = mx;
-        this.y = my;
-        this.ctx.drawImage(img, mx, my, this.width,this.height);
-    };
     
-    this.reveal = function(valAscend){
-        this.ctx.drawImage(img, this.x, valAscend, this.width, this.height);
+    this.reveal = function(){
+        this.ctx.drawImage(imgJackpot, this.x, startHeight, this.height, this.height);
     };
-    this.dereveal = function(valDescend){
-        this.ctx.drawImage(img,this.x, valDescend, this.width, this.height);
-    };
+       
 };
 
-var fibonaci = new Array();
-function getFibo(sumHalf){
-    var val,first,second,count;
-    first = 1;
-    second = 1;
-    count = 0;
-    for(;count <= sumHalf;){
-        count++;
-        val = first + second;
-        first = second;
-        second = val;
-    }
-    return second;
-};
+var highscore = 0;
+var score = 0;
 
-var firtsFixed,secondFixed;
+var firtsTar,secondTar,firtsFix,SecondFix;
+var deltaMove,deltaTime,deltaWidth;
+
+var dump;
 
 function genMove(){
-    console.log("genMove is runing");
-    var firtsTarget = Math.floor(Math.random() * boxes.length);
-    var secondTarget;
+    speed = 8;
+    exactDir();
+    
+    deltaTime = 20;
+    deltaMove = null;
+    deltaWidth = null;
+    firtsTar = null;
+    secondTar = null;
+    firtsFix = null;
+    secondFix = null;
+    midWidth = null;
+    
+    firtsTar = Math.floor(Math.random() * boxes.length);
     do{
-        secondTarget = Math.floor(Math.random() * boxes.length);
-    }while(secondTarget === null || secondTarget === firtsTarget);
-    console.log("Targets is " + firtsTarget+" and "+secondTarget);
-    if(boxes[firtsTarget].x > boxes[secondTarget].x){
-        deltaMove = boxes[firtsTarget].x - boxes[secondTarget].x;
-        secondFixed = firtsTarget;
-        firtsFixed= secondTarget;
+        secondTar = Math.floor(Math.random() * boxes.length);
+    }while(firtsTar === secondTar);
+    
+    
+    if(boxes[firtsTar].base < boxes[secondTar].base){
+        firtsFix = firtsTar;
+        secondFix = secondTar;
+        deltaWidth = boxes[firtsFix].x - boxes[secondFix].x;
     }else{
-        deltaMove = boxes[secondTarget].x - boxes[firtsTarget].x;
-        firtsFixed = firtsTarget;
-        secondFixed = secondTarget;
+        dump = secondTar;
+        secondFix = firtsTar;
+        firtsFix = dump;
+        deltaWidth = boxes[secondFix].x - boxes[firtsFix].x;
     }
-    console.log("fixed target are "+firtsFixed+" and " + secondFixed);
-    console.log("DELTAMOVE IS "+deltaMove);
-};
+    
+    dump = boxes[firtsFix].base;
+    boxes[firtsFix].base = boxes[secondFix].base;
+    boxes[secondFix].base = dump;
+    
+    deltaMove = deltaWidth/deltaTime;
+    if(dirMove === true){
+        dirMove = false;
+    }else{
+        dirMove = true;
+    }
+    
+}
 
-var timeMoveCont;
+function displayPoint(){
+    if(score > highscore){
+        highscore = score;
+    }
+    var canvas = document.getElementById("master");
+    var dispX = startWidth;
+    var dispY = devHeight;
+    var ctx = canvas.getContext("2d"); 
+    ctx.fillStyle = "#FFFFFF";
+    ctx.font = "40px Arial";
+    ctx.fillText("Highscore : "+highscore, dispX - 10, dispY - 65);
+    ctx.fillText("Score       : "+score, dispX - 10, dispY - 30);
+    
+}
+
+var starting = true;
+var speed = 200;
+var isTimeMove;
+
+var midWidth;
+var dirMove = true;
+
+var time = 1;
+var counter = 3;
+var timeCounter = 0;
+var isOnMoving, encounter;
+
+var isStart = true;
+var movePass;
+var gettingMove = null;
+
+var resseting, getStart, changePass, currentScore;
+var nextLevelPass, isFalse, getResume, listeningResume;
+var listening, getNextLevel, listeningMaster;
+
+
+var isZonk = function(){
+    this.x;
+    this.y;
+    this.w;
+    this.h;
+    this.canvas = document.getElementById("master");
+    this.ctx = this.canvas.getContext("2d");
+    this.display = function(){
+        this.ctx.drawImage(imgScare, this.x, this.y, this.w, this.h);
+    };
+};
+var getZonk = new isZonk();
+var trigger;
 
 function timeMove(){
-    console.log("timeMove is running");
-    timeMoveCont++;
-    if(boxes[firtsFixed].x >= layout[secondFixed].x){
-        clearInterval(master.moving);
+
+    master.clear();
+    if(boxes[firtsFix].x >= layout[(boxes[firtsFix].base)].x){
+            
+            if(timeCounter > counter){
+                if(encounter){
+                    for(i = 0; i < boxes.length; i++){
+                        boxes[i].y = startHeight;
+                    }
+                    encounter = false;
+                }
+                movePass = false;
+                if( ! movePass && gettingMove === null ){
+                    gettingMove = false;
+                    time++;
+                    score++;
+                        counter += time;
+                    listeningMaster = true;
+                    
+                }           
+                exactDir();
+                if(starting){
+                    nextLevelPass = true;      
+                }
+                
+                if(gettingMove === false){
+                    if( ! starting){
+                        displayPoint();
+                    }
+                    if(nextLevelPass){
+                        changePass = true;
+                        if(boxes[1].y >= devHeight*(5/100)){
+                            for(i = 0; i <= boxes.length; i++){
+                                boxes[i].y -= 4;
+                                if(i === 0){
+                                    boxes[i].reveal();
+                    
+                                }
+                                boxes[i].display();
+                            }
+                            if( ! starting){
+                                displayPoint();
+                            }
+                            
+                        }else{
+                            boxes[0].reveal();
+                            listening = true;
+                            
+                            if(isFalse && starting === false){
+                            
+                            if(changePass){
+                                currentScore = score;
+                                changePass = false;
+                                
+                            }
+                                
+                            if(score > highscore){
+                                    highscore = score;
+                                
+                                }
+                                var canvas = document.getElementById("master");
+                                    var ctx = canvas.getContext("2d");
+                                    var dispX = startWidth;
+                                    var dispY = startHeight;
+                                    ctx.fillStyle = "#FFFFFF";
+                                    ctx.font = "40px Arial";
+                                    ctx.fillText("GAMEOVER", dispX + 50,dispY - 4);
+                                    
+                                    ctx.fillRect(dispX, dispY, layWidth, layHeight);
+                                    ctx.fillStyle = "#000000";
+                                    ctx.font = "30px Arial";
+                                    ctx.fillText("HighScore  :  " + highscore, dispX + 38,dispY + 30);
+                                    ctx.fillText("Score      :  " + currentScore, dispX + 38,dispY + 65);
+                                    if(time > 5){
+                                        ctx.drawImage(imgScare,0,0,devWidth,devHeight);
+                                        trigger = true;
+                                    }
+                                    
+                            }
+                            if(starting){
+                                
+                                var canvas = document.getElementById("master");
+                                var dispX = startWidth;
+                                var dispY = startHeight;
+                                    var ctx = canvas.getContext("2d"); 
+                                    ctx.fillStyle = "#FFFFFF";
+                                    ctx.font = "40px Arial";
+                                    ctx.fillText("Tap to Start !", dispX + 30, dispY + 350);
+                            }
+                            
+                            if(getNextLevel){
+                                if(isFalse){
+                                    score = 0;
+                                    resseting = true;
+                                }if(starting){
+                                    highscore = 0;
+                                    resseting =true;
+                                    starting = false;
+                                }
+                                
+                                gettingMove = true;
+                                nextLevelPass = false;
+                                listeningMaster = false;
+                                listening = false;
+                                getNextLevel = false;
+                            }
+                        }
+                    }
+                }else{
+                    if( ! starting){
+                                displayPoint();
+                            }else{
+                                score = 0;
+                            }
+                    if(boxes[1].y <= startHeight){
+                        for(i = 0; i <= boxes.length; i++){
+                            boxes[i].y += 4;
+                            if(i === 0){
+                                boxes[i].reveal();
+
+                            }
+                            boxes[i].display();
+                        }
+                    }else{
+                        
+                        counter += time;
+                        timeCounter = 0;
+                        
+                        gettingMove = null;
+                        movePass = true;
+                    }
+                    
+                    
+                }
+                
+            }else{
+                movePass = true;
+            }
+        
+            if(movePass){
+                for(i = 0; i < boxes.length; i++){
+                    boxes[i].y = startHeight;
+                }
+                timeCounter++;
+                
+                if(resseting){
+                    if(starting){
+                        highscore = 0;
+                    }
+                    starting = false;
+                    timeCounter = 0;
+                    time = 1;
+                    counter = 3;
+                    isFalse = false;
+                    resseting = false;
+                }
+                
+                genMove();
+            }
+        
+        
+    
     }else{
-        boxes[firtsFixed].move(boxes[firtsFixed].x += 5,0 );
-        boxes[secondFixed].move(boxes[secondFixed].x -= 5,0 );
-    }
-    for(i = 0; i <= boxes.length; i++){
-        if( i === firtsFixed || i === secondFixed){
-            continue;
+        encounter = true;
+        if( ! starting){
+        
+            displayPoint();
         }else{
-            boxes[i].display();
+            score = 0;
+        }
+        
+            if(dirMove === true){
+                boxes[firtsFix].x += devWidth/120;
+                boxes[secondFix].x -= devWidth/120;
+                if(boxes[firtsFix].x <=  (boxes[firtsFix].x + ((boxes[secondFix].x - boxes[firtsFix].x)/2))){
+                    boxes[firtsFix].y += devHeight*(0.8/100);
+                    boxes[secondFix].y -= devHeight*(0.8/100);
+                }else{
+                    boxes[firtsFix].y -= devHeight*(0.8/100);
+                    boxes[secondFix].y += devHeight*(0.8/100);
+                }
+            }else{
+                boxes[firtsFix].x += devWidth/120;
+                boxes[secondFix].x -= devWidth/120;
+                if(boxes[firtsFix].x <=  (boxes[firtsFix].x + ((boxes[secondFix].x - boxes[firtsFix].x)/2))){
+                    boxes[firtsFix].y -= devHeight*(0.8/100);
+                    boxes[secondFix].y += devHeight*(0.8/100);
+                }else{
+                    boxes[firtsFix].y += devHeight*(0.8/100);
+                    boxes[secondFix].y -= devHeight*(0.8/100);
+                }
+            }
+        
+    }
+    
+    for(i = 0; i <= boxes.length; i++ ){
+        boxes[i].display();
+    }
+    
+}
+
+var isStop;
+
+ window.addEventListener("click",function(e){
+    if(listening === true){
+        getNextLevel = true;
+        
+    }
+    if(listeningResume){
+        getResume = true;
+        
+    }
+    
+    var mouse = {
+        x: e.pageX,
+        y: e.pageY
+    };
+    for(i = 0; i <= boxes.length; i++){
+        if(boxes[i].x <= mouse.x && mouse.x <= boxes[i].x + layout[i].size_X &&
+                boxes[i].y <= mouse.y && mouse.y <= boxes[i].y + layout[i].size_Y){
+                if(i === 0 && listeningMaster){
+                    nextLevelPass = true;
+                }else{
+                    nextLevelPass = true;
+                    isFalse = true;
+                    
+                }
         }
     }
+});
+
+
+function exactDir(){
+    for(i = 0; i < boxes.length; i++){
+        boxes[i].x = layout[boxes[i].base].x;
+    
+    }
 }
+
